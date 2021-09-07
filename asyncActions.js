9975341f22c21604bf9234738252e97474e56c91
Sudip Pahari <sudippahari1@gmail.com>
2021-09-07 11:40:39 +0530

    second commit

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,9 @@
 const redux= require('redux');
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require('redux-thunk').default;
+const axios = require('axios');
+
 const initialState = {
     loading: false,
     users:[],
@@ -52,4 +56,23 @@ const reducer= (state= initialState, action)=>{
             }
     }
 }
-const store = createStore(reducer)
+
+const fetchUsers=()=>{
+    return function(dispatch){
+        dispatch(fetchUsersRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response=>{
+            //response.data is the array of users
+            const users= response.data.map(user=> user.id)
+            dispatch (fetchUsersRequest(users))
+        })
+        .catch(error=>{
+            dispatch(fetchUsersFailure(error.message))
+        })
+    }
+}
+
+const store = createStore(reducer , applyMiddleware(thunkMiddleware))
+
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(fetchUsers())
